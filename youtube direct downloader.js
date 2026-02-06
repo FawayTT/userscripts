56d89d29bb8f43cd4b146d0ac7b39df5b4593946
C4pt41nN3mo <69541305+C4pt41nN3mo@users.noreply.github.com>
2026-02-06 09:51:39 -0800

    removed debug logs

--- youtube direct downloader.js
+++ youtube direct downloader.js
@@ -400,17 +400,14 @@ const downloadServices = {
             document.querySelectorAll('button:not(#submit)')[audioOnly ? 1 : 0].id = '';
 
             convertButton.click();
-            console.log("Clicked Convert!");
 
             const ytmp3Observer = new MutationObserver( function () {
               const finishedConversion = document.querySelector('div[style="justify-content: center;"');
               if (finishedConversion) {
-                console.log("conversion finished, clicking download");
                 finishedConversion.querySelector('button').click();
               }
             });
 
-            console.log("Saw convert button was clicked, waiting for conversion to complete");
             ytmp3Observer.observe(document.body, {
             childList: true,
             subtree: true,});
